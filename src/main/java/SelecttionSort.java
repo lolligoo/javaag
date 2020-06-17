b6af903db06c1d5c59c771737677721c5e0ad133
lolligoo <lolligoo@gmail.com>
2020-06-17 09:09:16 +0800

    选择排序

--- src/main/java/SelecttionSort.java
+++ src/main/java/SelecttionSort.java
@@ -1,25 +1,25 @@
 public class SelecttionSort {
     public static int[] SelectSort(int[] arr){
-        int small = arr[0];
-        int smallindex = 0;
-
-        for(int i = 0; i < arr.length; i++){
-            if(small > arr[i]){
-                small = arr[i];
-                smallindex = i;
+        int small;
+        int small_index = 0;
+        for(int j = 0; j < arr.length; j++){
+            small = arr[j];
+            for(int i = j; i < arr.length; i++){
+                if(small >= arr[i]){
+                    small = arr[i];
+                    small_index = i;
+                }
             }
+            arr[small_index] = arr[j];
+            arr[j] = small;
         }
-
-        arr[smallindex] = arr[0];
-        arr[0] = small;
-
         return arr;
     }
     public static void main (String[] arg){
-        int[] arr = {2, 5, 9, 7, 3};
+        int[] arr = {2, 5, 9, 7, 3,78, 76, 56, 34, 80, 12,4343, 54547, 89653};
         int[] newarr = SelectSort(arr);
         for(int i : newarr){
-            System.out.print(i);
+            System.out.print(i+" ");
         }
     }
 }
